84ceb96e9b62a0d035987f06fc9f92009e902df0
Ilya Sher <ilya.sher@coding-knight.com>
2014-02-03 14:16:00 +0200

    improve test code

--- cloudify_plugin_openstack_neutron_provisioner/tests/test.py
+++ cloudify_plugin_openstack_neutron_provisioner/tests/test.py
@@ -10,29 +10,28 @@ import cloudify_plugin_openstack_neutron_provisioner.network as cfy_net
 
 class OpenstackNeutronTest(os_common.TestCase):
 
-    @os_common.with_neutron_client
-    def test_port(self, neutron_client):
+    def test_port(self):
         name = self.name_prefix + 'the_port'
         network = self.create_network('for_port')
         subnet = self.create_subnet('for_port', '10.11.12.0/24', network=network)
 
-        self.assertEquals(0, len(list(neutron_client.cosmo_list('port', name=name))))
+        self.assertThereIsNo('port', name=name)
 
+        __cloudify_id = '__cloudify_id_' + name + '_port'
         __target_cloudify_id = '__cloudify_id_' + name + '_net'
         network_node_state = self.nodes_data[__target_cloudify_id] 
         network_node_state['external_id'] = network['id']
 
         cfy_port.create_in_network(
-            __source_properties={'port': {'name': name}},
+            __source_properties = {'port': {'name': name}},
             __target_cloudify_id = __target_cloudify_id
         )
-        self.assertEquals(1, len(list(neutron_client.cosmo_list('port', name=name))))
+        self.assertThereIsOne('port', name=name)
 
         cfy_port.delete(port={'name': name})
-        self.assertEquals(0, len(list(neutron_client.cosmo_list('port', name=name))))
+        self.assertThereIsNo('port', name=name)
 
-    @os_common.with_neutron_client
-    def test_network(self, neutron_client):
+    def test_network(self):
         name = self.name_prefix + 'net'
         network = {'name': name}
 
