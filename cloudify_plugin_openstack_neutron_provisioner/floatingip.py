f0b839fdf8627fcf9d293e0ed80067f35ef18af1
Ilya Sher <ilya.sher@coding-knight.com>
2014-02-16 14:02:43 +0200

    work on floatingip and port

--- cloudify_plugin_openstack_neutron_provisioner/floatingip.py
+++ cloudify_plugin_openstack_neutron_provisioner/floatingip.py
@@ -3,13 +3,34 @@
 from cloudify.decorators import operation
 from cosmo_plugin_openstack_common import with_neutron_client
 
+# Create or use existing
 @operation
 @with_neutron_client
 def create(ctx, neutron_client, **kwargs):
+
+    # Already acquired?
+    if ctx.runtime_properties.get('external_id'):
+        ctx.logger.debug("Using already allocated Floating IP {0}".format(ctx.runtime_properties['floating_ip_address']))
+        ctx.set_started()
+        return
+
     floatingip = {
         # No defaults
     }
     floatingip.update(ctx.properties['floatingip'])
+
+    # Sugar: ip -> (copy as is) -> floating_ip_address
+    if 'ip' in floatingip:
+        floatingip['floating_ip_address'] = floatingip['ip']
+        del floatingip['ip']
+
+    if 'floating_ip_address' in floatingip:
+        fip = neutron_client.cosmo_get('floatingip', floating_ip_address=floatingip['floating_ip_address'])
+        ctx.runtime_properties['external_id'] = fip['id']
+        ctx.set_started()
+        return
+
+    # Sugar: floating_network_name -> (resolve) -> floating_network_id
     if 'floating_network_name' in floatingip:
         floatingip['floating_network_id'] = neutron_client.cosmo_get_named('network', floatingip['floating_network_name'])
         del floatingip['floating_network_name']
--- cloudify_plugin_openstack_neutron_provisioner/port.py
+++ cloudify_plugin_openstack_neutron_provisioner/port.py
@@ -3,12 +3,25 @@
 from cloudify.decorators import operation
 from cosmo_plugin_openstack_common import with_neutron_client
 
+def _find_network_in_related_nodes(ctx, neutron_client):
+    networks_ids = [n['id'] for n in neutron_client.list_networks()['networks']]
+    ret = []
+    for runtime_properties in ctx.capabilities.get_all().values():
+        external_id = runtime_properties.get('external_id')
+        if external_id in networks_ids:
+            ret.append(external_id)
+    if len(ret) != 1:
+        # TODO: better message
+        raise RuntimeError("Failed to find port's network")
+    return ret[0]
+
+
 @operation
 @with_neutron_client
 def create(ctx, neutron_client, **kwargs):
     port = {
         'name': ctx.node_id,
-        'network_id': ctx.capabilities['external_id'],
+        'network_id': _find_network_in_related_nodes(ctx, neutron_client),
     }
     port.update(ctx.properties['port'])
     p = neutron_client.create_port({'port': port})['port']
@@ -17,5 +30,15 @@ def create(ctx, neutron_client, **kwargs):
 
 @operation
 @with_neutron_client
-def delete(port, neutron_client, **kwargs):
+def delete(ctx, neutron_client, **kwargs):
     neutron_client.delete_port(ctx.runtime_properties['external_id'])
+    ctx.set_stopped()
+
+@operation
+@with_neutron_client
+def connect_security_group(ctx, neutron_client, **kwargs):
+    # WARNING: non-atomic operation
+    port = neutron_client.cosmo_get('port', id=ctx.runtime_properties['external_id'])
+    ctx.logger.info("connect_security_group(): related={0}".format(ctx.related.runtime_properties))
+    sgs = port['security_groups'] + [ctx.related.runtime_properties['external_id']]
+    neutron.update_port(port_id, {'port': {'security_groups': sgs}})
